ca66e7d80709ad28821aa556bfeb7d94eb236129
ymasog <yer.masog@gmail.com>
2021-01-12 18:51:53 -0600

    added for loop to highlight the hour of the day

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -1,15 +1,51 @@
-var date = moment().format("MMM Do YYYY");
+var date = moment().format("MMM Do YYYY, h:mm a");
 $("#currentDay").append(date);
 
-$(".saveBtn").on("click", function(){
-    var text = $(".input").val();
+var input1 = $("#9")
+// var movie = $(this).attr("data-name");
 
-    localStorage.setItem("input", text);
-    
+$("#btn1").on("click", function(event){
+    event.preventDefault;
+    var textOne = $("#9").val();
+    localStorage.setItem("btn1", textOne); 
 })
+input1 = localStorage.getItem("btn1");
 
 
 
-//$(this).prev().val()
+// var currentData = $()
+// var saveBtn = $(".saveBtn");
+
+// for (let index = 0; index < saveBtn.length; index++) {
+//     $(saveBtn[index]).on("click", function() {
+//         var thisDataHour = $(this).attr("data-hour");
+//         var saveToDo = $("#" + thisDataHour).val();
+
+//         var toDoList = {
+//             time: thisDataHour,
+//             toDo: saveToDo
+//         }
+//         currentData.push(toDoList);
+//         localStorage.setItem("Schedule", JSON.stringify(currentData))
+//     })
+    
+// }
+
+
+
+var toDoItems = $(".textarea")
+
+for (let index = 0; index < toDoItems.length; index++) {
+    var timeNow = parseInt($(toDoItems[index]).attr("id"));
+    var compareHour = parseInt(moment().format("HH"));
+
+        if(timeNow<compareHour) {
+            $(toDoItems[index]).addClass("past");
+        }else if(timeNow>compareHour) {
+            ($(toDoItems[index]).addClass("future"))
+        }else ($(toDoItems[index]).addClass("present"))
+    
+    
+}
 
 
